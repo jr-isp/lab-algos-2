debd76ca52a4fbfb8f88991313ed710381495ee7
Jesús Isea <163884862+jr-isp@users.noreply.github.com>
2024-05-02 21:11:51 -0400

    Update life.cpp

--- semana 1/life.cpp
+++ semana 1/life.cpp
@@ -3,6 +3,11 @@ la vida, el universo y todo lo demás de SPOJ.
 
 Hecho por Jesús Isea 16-10553*/
 
+/*
+compara las entradas de cin con 42, si no coincide entonces la imprime de vuelta. Si coincide,
+entonces se detiene el programa.
+*/
+
 #include <iostream>
 using namespace std;
 
